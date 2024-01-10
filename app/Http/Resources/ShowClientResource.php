aba4f3181698f5b5cf05b73ce436facd66add961
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-01-10 22:40:05 +0500

    Small Changes

--- app/Http/Resources/ShowClientResource.php
+++ app/Http/Resources/ShowClientResource.php
@@ -15,6 +15,7 @@ class ShowClientResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id ,
             'name' => $this->name ,
             'phone_number' => $this->phone_number
         ];
