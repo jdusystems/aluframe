ecba905a65aa0a0a23c276dd850606a915fc544a
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-01-16 11:58:07 +0500

    Small changes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -40,6 +35,7 @@ Route::middleware(['auth:sanctum'])->group( function () {
 
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
     Route::post('/change-password', [PasswordResetController::class, 'changePassword'])
         ->name('change.password');
 
@@ -48,7 +44,6 @@ Route::middleware(['auth:sanctum'])->group( function () {
     });
 
 });
-
 
 Route::apiResource('profile-colors' , ProfileColorController::class);
 Route::apiResource('window-colors' , WindowColorController::class);
@@ -80,34 +75,3 @@ Route::get('/image' , function (){
 Route::apiResource('posts' , PostController::class);
 
 
-
-// Auth User
-
-
-Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.email');
-
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.store');
-
-Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-    ->middleware(['auth', 'signed', 'throttle:6,1'])
-    ->name('verification.verify');
-
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth', 'throttle:6,1'])
-    ->name('verification.send');
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
