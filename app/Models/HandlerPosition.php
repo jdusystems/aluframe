0f4aae2ca8ac204f2bdd1ccee488e87a0c024c03
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-01-27 14:54:24 +0500

    New changes

--- app/Models/HandlerPosition.php
+++ app/Models/HandlerPosition.php
@@ -10,7 +10,7 @@ class HandlerPosition extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name' , 'image_name' , 'image_url'
+        'name' , 'image_name' , 'image_url' , 'sort_index'
     ];
 
     public function openingTypes(){
