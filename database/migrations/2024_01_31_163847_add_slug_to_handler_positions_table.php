a9a93d185795da49ae24f69bbb33ae456df97278
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-01-31 22:29:40 +0500

    Order finished

--- database/migrations/2024_01_31_163847_add_slug_to_handler_positions_table.php
+++ database/migrations/2024_01_31_163847_add_slug_to_handler_positions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('handler_positions', function (Blueprint $table) {
-            $table->string('slug')->nullable();
+            $table->string('slug')->nullable()->after('name');
         });
     }
 
