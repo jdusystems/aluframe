48d45a18c55ee04bf4722b08e30e63c53c5c352c
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-02-09 19:20:39 +0500

    New changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -301,7 +301,7 @@ class OrderController extends Controller
         $perimeter = 2*($width + $height);
 
         if($request->has('profile_type_id')){
-            $profileType = ProfileType::where('id' , $request->profile_type_id)->whereNull('deleted_at')->first();
+            $profileType = ProfileType::find($request->profile_type_id);
             if($profileType){
                 if($profileType->selant){
                     $sealant  = Sealant::where('profile_type_id' , $profileType->id)->whereNull('deleted_at')->first();
