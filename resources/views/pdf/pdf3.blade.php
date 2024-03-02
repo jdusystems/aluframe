454d8ce295487f735174e275d47cc2fbe552ac5d
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-02 19:04:36 +0500

    New changes

--- resources/views/pdf/pdf3.blade.php
+++ resources/views/pdf/pdf3.blade.php
@@ -18,9 +18,9 @@
           margin: 0;
           box-sizing: border-box;
       }
-      .break-page{
-          page-break-after: always;
-      }
+      /*.break-page{*/
+      /*    page-break-after: always;*/
+      /*}*/
 
       body{
           font-family: "Inter";
