1b6d1f391054249ac0b741902b4f628deff3cbbc
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-04 09:45:28 +0500

    New changes

--- resources/views/pdf/pdf3.blade.php
+++ resources/views/pdf/pdf3.blade.php
@@ -236,153 +236,154 @@
 </head>
 
 <body>
-<div class="full">
-    <div class="wrap">
-        @foreach($orderDetails as $orderDetail)
-            <table class="card">
-                <tr class="card-item">
-                    <td class="text">Профиль:</td>
-                    <td class="title">{{($orderDetail->profileType) ? $orderDetail->profileType->name: ""}}, {{($orderDetail->profileColor) ? $orderDetail->profileColor->name : ""}}</td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Цвет стекла:</td>
-                    <td class="title">{{($orderDetail->windowColor) ? $orderDetail->windowColor->name :""}}</td>
-                </tr>
-                <tr class="list-item">
-                    <td class="text">Дополнительные услуги для стекла:</td>
-                    <td class="title">
-                        @foreach($orderDetail->additionalServices as $additionalService)
-                            {{($additionalService) ? $additionalService->name: ""}} ,
-                        @endforeach
-                    </td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Тип открывания:</td>
-                    <td class="title">{{($orderDetail->openingType) ? $orderDetail->openingType->name : ""}}</td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Количество петель:</td>
-                    <td class="title">{{$orderDetail->number_of_loops}}</td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Высота:</td>
-                    <td class="title">{{$orderDetail->height}}</td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Ширина:</td>
-                    <td class="title">{{$orderDetail->width}}</td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Кол-во L:</td>
-                    <td class="title">{{$orderDetail->quantity_left}}</td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Кол-во R:</td>
-                    <td class="title">{{$orderDetail->quantity_right}}</td>
-                </tr>
-                <tr class="card-item">
-                        <?php
-                            $windowHandler = \App\Models\WindowHandler::where('profile_type_id' , $orderDetail->profile_type_id)->where('profile_color_id' , $orderDetail->profile_color_id)->whereNull('deleted_at')->first();
-                        ?>
-                    <td class="text">Ручка:</td>
-                    <td class="title">
-                        {{($windowHandler) ? $windowHandler->name : ""}}
-                    </td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Присака станд.?:</td>
-                    <td class="title">{{($orderDetail->additive_sizes) ? $orderDetail->additive_sizes : ""}} </td>
-                </tr>
-                <tr class="card-item">
-                    <td class="text">Комментарий:</td>
-                    <td class="title">{{ ($orderDetail->comment) ? $orderDetail->comment : ""}}</td>
-                </tr>
-            </table>
-        @endforeach
-    </div>
-</div>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<div class="wrap1">
-    <div class="card1">
-            <table class="card-item1">
-                <tr class="card-title" style="margin-left: 20px">
-                    <td style=" padding-left: 30px;">Профиль</td>
-                    <td style=" padding-left: 20px;">{{$order->order_id}}</td>
-                    <td style=" padding-left: 20px;">{{$order->created_at}}</td>
-                </tr>
-                <tr class="card-top">
-                    <td style=" padding-left: 30px;">Код товара</td>
-                    <td style=" padding-left: 20px;">Высота</td>
-                    <td style=" padding-left: 20px;">Кол-во</td>
-                </tr>
-             @foreach($profiles as $profile)
-                <tr class="card-list">
-                    <td style=" padding-left: 30px;">{{$profile->profileType->name}}</td>
-                    <td style=" padding-left: 20px;">{{2*($profile->total_height + $profile->total_width)}}</td>
-                    <td style=" padding-left: 20px;">{{$profile->total_facade_quantity}}</td>
-                </tr>
-                 @endforeach
-            </table>
-    </div>
-    <div class="card2">
-        <table class="card-item1">
-            <tr class="card-title" >
-                <td style="padding-left: 30px;">Стекло</td>
-                <td style=" padding-left: 20px;">{{$order->order_id}}</td>
-                <td style=" padding-left: 20px; " colspan="2">{{$order->created_at}}</td>
+{{--<div class="full">--}}
+{{--    <div class="wrap">--}}
+{{--        @foreach($orderDetails as $orderDetail)--}}
+{{--            <table class="card">--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Профиль:</td>--}}
+{{--                    <td class="title">{{($orderDetail->profileType) ? $orderDetail->profileType->name: ""}}, {{($orderDetail->profileColor) ? $orderDetail->profileColor->name : ""}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Цвет стекла:</td>--}}
+{{--                    <td class="title">{{($orderDetail->windowColor) ? $orderDetail->windowColor->name :""}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="list-item">--}}
+{{--                    <td class="text">Дополнительные услуги для стекла:</td>--}}
+{{--                    <td class="title">--}}
+{{--                        @foreach($orderDetail->additionalServices as $additionalService)--}}
+{{--                            {{($additionalService) ? $additionalService->name: ""}} ,--}}
+{{--                        @endforeach--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Тип открывания:</td>--}}
+{{--                    <td class="title">{{($orderDetail->openingType) ? $orderDetail->openingType->name : ""}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Количество петель:</td>--}}
+{{--                    <td class="title">{{$orderDetail->number_of_loops}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Высота:</td>--}}
+{{--                    <td class="title">{{$orderDetail->height}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Ширина:</td>--}}
+{{--                    <td class="title">{{$orderDetail->width}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Кол-во L:</td>--}}
+{{--                    <td class="title">{{$orderDetail->quantity_left}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Кол-во R:</td>--}}
+{{--                    <td class="title">{{$orderDetail->quantity_right}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                        <?php--}}
+{{--                            $windowHandler = \App\Models\WindowHandler::where('profile_type_id' , $orderDetail->profile_type_id)->where('profile_color_id' , $orderDetail->profile_color_id)->whereNull('deleted_at')->first();--}}
+{{--                        ?>--}}
+{{--                    <td class="text">Ручка:</td>--}}
+{{--                    <td class="title">--}}
+{{--                        {{($windowHandler) ? $windowHandler->name : ""}}--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Присака станд.?:</td>--}}
+{{--                    <td class="title">{{($orderDetail->additive_sizes) ? $orderDetail->additive_sizes : ""}} </td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-item">--}}
+{{--                    <td class="text">Комментарий:</td>--}}
+{{--                    <td class="title">{{ ($orderDetail->comment) ? $orderDetail->comment : ""}}</td>--}}
+{{--                </tr>--}}
+{{--            </table>--}}
+{{--        @endforeach--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<div class="wrap1">--}}
+{{--    <div class="card1">--}}
+{{--            <table class="card-item1">--}}
+{{--                <tr class="card-title" style="margin-left: 20px">--}}
+{{--                    <td style=" padding-left: 30px;">Профиль</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{$order->order_id}}</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{$order->created_at}}</td>--}}
+{{--                </tr>--}}
+{{--                <tr class="card-top">--}}
+{{--                    <td style=" padding-left: 30px;">Код товара</td>--}}
+{{--                    <td style=" padding-left: 20px;">Высота</td>--}}
+{{--                    <td style=" padding-left: 20px;">Кол-во</td>--}}
+{{--                </tr>--}}
+{{--             @foreach($profiles as $profile)--}}
+{{--                <tr class="card-list">--}}
+{{--                    <td style=" padding-left: 30px;">{{$profile->profileType->name}}</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{2*($profile->total_height + $profile->total_width)}}</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{$profile->total_facade_quantity}}</td>--}}
+{{--                </tr>--}}
+{{--                 @endforeach--}}
+{{--            </table>--}}
+{{--    </div>--}}
+{{--    <div class="card2">--}}
+{{--        <table class="card-item1">--}}
+{{--            <tr class="card-title" >--}}
+{{--                <td style="padding-left: 30px;">Стекло</td>--}}
+{{--                <td style=" padding-left: 20px;">{{$order->order_id}}</td>--}}
+{{--                <td style=" padding-left: 20px; " colspan="2">{{$order->created_at}}</td>--}}
 
-            </tr>
-            <tr class="card-top">
-                <td style=" padding-left: 30px;">Код товара</td>
-                <td style=" padding-left: 20px;">Высота</td>
-                <td style=" padding-left: 20px;">Ширина</td>
-                <td style=" padding-left: 20px;">Кол-во</td>
-            </tr>
-            @foreach($windowColors as $windowColor)
-                    <?php
-                    $profiles = \App\Models\OrderDetail::where('window_color_id' , $windowColor->window_color_id)->where('order_id' , $order->id)->get();
-                    ?>
-                <tr class="card-list">
-                    <td style=" padding-left: 30px;">{{$windowColor->windowColor->name}}</td>
-                    <td style=" padding-left: 20px;">{{$windowColor->total_width}}</td>
-                    <td style=" padding-left: 20px;">{{$windowColor->total_height}}</td>
-                    <td style=" padding-left: 20px;">{{$windowColor->total_facade_quantity}}</td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
-</div>
+{{--            </tr>--}}
+{{--            <tr class="card-top">--}}
+{{--                <td style=" padding-left: 30px;">Код товара</td>--}}
+{{--                <td style=" padding-left: 20px;">Высота</td>--}}
+{{--                <td style=" padding-left: 20px;">Ширина</td>--}}
+{{--                <td style=" padding-left: 20px;">Кол-во</td>--}}
+{{--            </tr>--}}
+{{--            @foreach($windowColors as $windowColor)--}}
+{{--                    <?php--}}
+{{--                    $profiles = \App\Models\OrderDetail::where('window_color_id' , $windowColor->window_color_id)->where('order_id' , $order->id)->get();--}}
+{{--                    ?>--}}
+{{--                <tr class="card-list">--}}
+{{--                    <td style=" padding-left: 30px;">{{$windowColor->windowColor->name}}</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{$windowColor->total_width}}</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{$windowColor->total_height}}</td>--}}
+{{--                    <td style=" padding-left: 20px;">{{$windowColor->total_facade_quantity}}</td>--}}
+{{--                </tr>--}}
+{{--            @endforeach--}}
+{{--        </table>--}}
+{{--    </div>--}}
+{{--</div>--}}
 
-<div class="break-page">
+{{--<div class="break-page">--}}
 
-</div>
-<br>
-<br>
-<br>
+{{--</div>--}}
+{{--<br>--}}
+{{--<br>--}}
+{{--<br>--}}
 <div class="wrap3">
+
     @foreach($orderDetails as $orderDetail)
             <?php
             $facades = $orderDetail->quantity_left + $orderDetail->quantity_right  + 1;
