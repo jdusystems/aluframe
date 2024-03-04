1d903c7f6d570b76839c0fdc8fadeca96e17fc4e
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-04 14:40:28 +0500

    PDF

--- resources/views/pdf/pdf3.blade.php
+++ resources/views/pdf/pdf3.blade.php
@@ -372,7 +372,7 @@
                 $facades = $orderDetail->quantity_left + $orderDetail->quantity_right;
             ?>
         @for($i = 1;$i <= $facades;$i++)
-            <table class="card3" @if($i%2==0) style="float: right" @endif>
+            <table class="card3" @if($i%2==0) style="float: right !important;" @endif>
                 <tr class="card-top3">
                     <td style="padding-right: 30px"><b>{{$orderDetail->id}}</b> {{$order->created_at}}</td>
                     <td style="padding-left: 20px">Фасад {{$i}}/{{$facades}}</td>
