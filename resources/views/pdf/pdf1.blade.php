36d63a7f582f6b16c3a49522f34610ec972b4456
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-07 23:57:28 +0500

    New changes

--- resources/views/pdf/pdf1.blade.php
+++ resources/views/pdf/pdf1.blade.php
@@ -185,9 +185,9 @@
 <div class="container">
     <div class="client" style="margin-right: 200px;" >
         <span class="text" style="font-weight: bold;">Заказчик:</span>
-        <span class="text" style="padding-left: 3px">{{$user->name}}, </span>
+        <span class="text" style="padding-left: 3px;max-width: 12px;">{{$user->name}}, </span>
         <span class="text" style="padding-left: 10px">+{{$user->phone_number}}</span>
-        <span class="text" style="margin-left: 250px">#{{$order->id}}</span>
+        <span class="text" style="margin-left: 220px">#{{$order->id}}</span>
         <span class="text" style="margin-left: 20px">{{$order->created_at->format('d.m.Y H:i')}}</span>
     </div>
     <header class="header">
