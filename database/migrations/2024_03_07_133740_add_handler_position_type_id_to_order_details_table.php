aa5215ef8b077de6a3a1f758ec59e158a4a89bc1
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-07 13:39:26 +0500

    Handler position

---
+++ database/migrations/2024_03_07_133740_add_handler_position_type_id_to_order_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('handler_position_type_id')->nullable();
+            $table->foreign('handler_position_type_id')->references('id')->on('handler_position_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
