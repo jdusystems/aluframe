16b9f3bb63b40c92ce6211035a693b34061081af
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-03-08 21:08:40 +0500

    New changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -52,12 +52,12 @@ class OrderController extends Controller
             }else{
                 $orders = Order::when($status, function ($query) use ($status) {
                     $query->where('status', $status);
+                })->when($name , function ($query) use ($name){
+                    $query->where('id' , $name);
                 })->when($name, function ($query) use ($name){
                     $query->whereHas('user', function ($subquery) use ($name) {
                         $subquery->where('name', 'like', '%' . $name . '%');
                     });
-                })->when($name , function ($query) use ($name){
-                  $query->where('id' , $name);
                 })->when($phoneNumber, function ($query) use ($phoneNumber){
                     $query->whereHas('user', function ($subquery) use ($phoneNumber) {
                         $subquery->where('phone_number', 'like', '%' . $phoneNumber . '%');
